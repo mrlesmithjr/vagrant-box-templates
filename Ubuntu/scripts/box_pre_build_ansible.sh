9319fef76d9315ea83adc855ca622eadf69a50d2
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-18 14:39:41 -0400

    added prompt for sudo password

--- Ubuntu/scripts/box_pre_build_ansible.sh
+++ Ubuntu/scripts/box_pre_build_ansible.sh
@@ -3,4 +3,4 @@
 sudo apt-get install -y python-pip python-dev
 sudo pip install ansible==1.9.1
 
-ansible-playbook -i hosts box_pre_build.yml --connection=local
+ansible-playbook -i hosts box_pre_build.yml --connection=local --ask-sudo-pass
