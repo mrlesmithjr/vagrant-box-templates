63afb922e300bd86bcaac05ee91be989ef0f105e
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-18 16:03:21 -0400

    moving zeroing out space out of Ansible

--- Ubuntu/scripts/box_pre_build_ansible.sh
+++ Ubuntu/scripts/box_pre_build_ansible.sh
@@ -4,3 +4,6 @@ sudo apt-get install -y python-pip python-dev
 sudo pip install ansible==1.9.1
 
 ansible-playbook -i hosts box_pre_build.yml --connection=local --ask-sudo-pass
+
+sudo dd if=/dev/zero of=/EMPTY bs=1M
+sudo rm -f /EMPTY
