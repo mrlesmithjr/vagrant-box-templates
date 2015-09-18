9cfa603a456e9e49b1159083fe44901a0b4b5af2
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-18 16:10:13 -0400

    added comments and Clear bash history

--- Ubuntu/scripts/box_pre_build_ansible.sh
+++ Ubuntu/scripts/box_pre_build_ansible.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Install Ansible to provision host
 sudo apt-get install -y python-pip python-dev
 sudo pip install ansible==1.9.1
 
+# Run Ansible provisioning
 ansible-playbook -i hosts box_pre_build.yml --connection=local --ask-sudo-pass
 
+# Zero out the free space to save space in the final image:
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
+
+# Clear Bash History
+cat /dev/null > ~/.bash_history && history -c && exit
