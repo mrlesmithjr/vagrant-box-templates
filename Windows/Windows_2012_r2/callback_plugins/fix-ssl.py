cbd6450d1c5714e7d9f268235d033ad627eb6d5b
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-18 22:33:42 -0400

    added windows 2012 r2

---
+++ Windows/Windows_2012_r2/callback_plugins/fix-ssl.py
@@ -0,0 +1,6 @@
+import ssl
+if hasattr(ssl, '_create_default_https_context') and hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+class CallbackModule(object):
+    pass
