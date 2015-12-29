132375acca2df2eb445ea7aadae5713eb6b79024
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-12-29 15:33:22 -0500

    modifying script for Debian
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- Debian/scripts/box_pre_build_ansible.sh
+++ Debian/scripts/box_pre_build_ansible.sh
@@ -2,10 +2,10 @@
 
 # Install Ansible to provision host
 sudo apt-get install -y python-pip python-dev
-sudo pip install ansible==1.9.1
+sudo pip install ansible
 
 # Run Ansible provisioning
-ansible-playbook -i hosts box_pre_build.yml --connection=local --ask-sudo-pass
+ansible-playbook -i "localhost," -c local box_pre_build.yml --ask-sudo-pass
 
 # Zero out the free space to save space in the final image:
 sudo dd if=/dev/zero of=/EMPTY bs=1M
