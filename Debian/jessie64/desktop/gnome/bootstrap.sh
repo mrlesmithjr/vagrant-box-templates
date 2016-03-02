e0262395cb5069fedc3734b9cf38bdd479928053
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-03-02 17:15:00 -0500

    Made script executable
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

--- Debian/jessie64/desktop/gnome/bootstrap.sh
+++ Debian/jessie64/desktop/gnome/bootstrap.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-apt-get update
+if [ -f /etc/debian_version ]; then
+  codename="$(lsb_release -c | awk {'print $2}')"
+  if [ $codename="vivid" ]; then
+    sudo apt-get update && sudo apt-get -y install python-simplejson
+  fi
+fi
