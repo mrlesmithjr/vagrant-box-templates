0724dfeaa3aab70fbe60dadcf2b492252768c7c6
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-12-12 14:17:18 -0500

    Cleaned up scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,6 @@ set -e
 if [ -f /etc/os-release ]; then
     os_name="$(awk -F= '/^NAME/{ print $2 }' /etc/os-release | sed 's/"//g')"
     os_version_id="$(awk -F= '/^VERSION_ID/{ print $2}' /etc/os-release | sed 's/"//g')"
-    echo $os_name
-    echo $os_version_id
+    echo "$os_name"
+    echo "$os_version_id"
 fi
