d6495fd80ed806760e913a7acf8d4a99e9bfb478
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-12-12 23:18:04 -0500

    Disabled cleaning up .vagrant dirs. They are added to .gitignore

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,8 +2,8 @@
 vagrant destroy -f
 find . -type d -name "host_vars"
 find . -type d -name "host_vars" -exec rm -rf {} +
-find . -type d -name ".vagrant"
-find . -type d -name ".vagrant" -exec rm -rf {} +
+# find . -type d -name ".vagrant"
+# find . -type d -name ".vagrant" -exec rm -rf {} +
 find . -type f -name "*.retry"
 find . -type f -name "*.retry" -exec rm {} +
 find . -type f -name "*.vmdk"
